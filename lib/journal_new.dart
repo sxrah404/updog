137874c9ff5782fd08957ded768f4c48ef82edf6
Sydney Hayosh <sydney@hayosh.com>
2025-11-23 18:46:26 -0600

    Added assets to new entry page

--- lib/journal_new.dart
+++ lib/journal_new.dart
@@ -11,24 +11,47 @@ class JournalNewEntryPage extends StatelessWidget {
 
     return Consumer<ThemeProvider>(
       builder: (context, themeProvider, _) {
-        return Row(
+        return Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            //Feeling image
             Image.asset(
               // turn this into button later
-              'assets/images/arrow.png',
+              'assets/images/other.png',
               fit: BoxFit.contain,
               height: screenHeight * 0.18,
             ),
-            Image.asset(
-              // turn this into button later
-              'assets/images/save.png',
-              fit: BoxFit.contain,
-              height: screenHeight * 0.18,
+
+            //Feeling text on the left and date on the right
+            Row(children: [],),
+
+            //Journal entry
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Type here'
+              )
+              
+            ),
+
+            //Back and save buttons
+            Row(
+              children: [
+                Image.asset(
+                  // turn this into button later
+                  'assets/images/arrow.png',
+                  fit: BoxFit.contain,
+                  height: screenHeight * 0.18,
+                ),
+                Image.asset(
+                  // turn this into button later
+                  'assets/images/save.png',
+                  fit: BoxFit.contain,
+                  height: screenHeight * 0.18,
+                ),
+              ],
             ),
           ],
         );
-        
       },
     );
   }
