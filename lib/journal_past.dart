49aeedb415ac84d4461c6ce3bc93831bee911b9b
Sarah Jabian <142950731+sxrah404@users.noreply.github.com>
2025-11-27 21:32:27 -0600

    added blank assets

--- lib/journal_past.dart
+++ lib/journal_past.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:updog/journal_new.dart';
 import 'theme_provider.dart';
-import 'journal_new.dart';
 
 class JournalPastEntriesPage extends StatefulWidget {
   final List<NewEntry> entries;
